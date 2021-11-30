446fb184fd96b3ca7fd1a96fa399b6c79528fef0
Leticia Honorio Pereira <79856171+Projetos-leticiahonorio@users.noreply.github.com>
2021-11-30 08:05:26 -0300

    Add files via upload

---
+++ exemplo_01.js
@@ -0,0 +1,9 @@
+texto = "vamos fazer uma calculadora"
+num1 = 5;
+num2 = 15;
+soma = num1+num2
+alert(soma);
+//document.write(texto);
+console.log(num1);
+
+document.getElementById('teste').innerHTML = texto;
